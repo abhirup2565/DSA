2c9651f68908a486debeec60fbdb57a3ee062429
abhirup2565 <abhirup2565@gmail.com>
2025-07-01 13:54:10 +0530

    Negative number in window

--- NegativeInWindow.java
+++ NegativeInWindow.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Solution {
+public class NegativeInWindow {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of array ");
@@ -23,37 +23,36 @@ public class Solution {
 
     static List<Integer> firstNegInt(int arr[], int k) {
         // write code here
-        List<Integer> new_arr = new ArrayList<Integer>();
-        
-        for(int i = 0 ; i<k;i++)
-        {
-            if(arr[i]<0)
+        Queue <Integer> negativeNumbers = new LinkedList<>();
+        List<Integer> answer = new ArrayList<>();
+        int windowSize=k;
+        int start=0,end=0;
+      
+      while(end<arr.length)
+      {
+          if(arr[end]<0)
+          {
+            negativeNumbers.add(arr[end]);
+          }
+          if(end-start+1==windowSize)
+          {
+            if(negativeNumbers.size()==0)
             {
-                new_arr.add(arr[i]);
+                answer.add(0);
             }
             else
             {
-                new_arr.add(0);
+                answer.add(negativeNumbers.peek());
             }
-        }
-        int i =1;
-        while(i<=arr.length-k)
-        {
-            if(arr[i+1]<0)
+            if(arr[start]<0)
             {
-                if(new_arr.get(i)>0)
-                {
-                    new_arr.set(i,arr[i+1]);
-                }
-                new_arr.add(arr[i+1]);
+                negativeNumbers.remove();
             }
-            else
-            {
-                new_arr.add(0);
-            }
-            
-            i++;
-        }
-        return new_arr;
+            start++;
+          }
+          end++;
+      }
+      return answer;
     }
-} 
+    }
+

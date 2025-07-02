b65ac474c38b7f65a141cddca2a416a4c2b84c5a
abhirup2565 <abhirup2565@gmail.com>
2025-07-02 18:40:47 +0530

    maximum Sliding

--- SlidingWindow/Maximum.java
+++ SlidingWindow/Maximum.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.util.Scanner;
 public class Maximum {
     public static void main(String[] args) {
@@ -24,17 +25,34 @@ public class Maximum {
 
     public static int[] maxSlidingWindow(int[] arr, int k) {
         int [] ans = new int[arr.length-k+1];
-        for(int i=0;i<=arr.length-k;i++)
-        {
-            int max =arr[i];
-            for(int j=i;j<i+k;j++)
+        int start=0,end=0,max=arr[0],windowSize=k;
+        Deque<Integer> temp = new ArrayDeque<>();
+        
+        while (end<arr.length) {
+            if(temp.size()==0||arr[end]<=temp.peekFirst())
             {
-                if(max<arr[j])
-                {
-                    max=arr[j];
-                }
+                temp.addFirst(arr[end]);
             }
-            ans[i]=max;
+            else {
+                while(temp.size()!=0 && arr[end]>temp.peekFirst())
+                {
+                temp.removeFirst();
+                }
+                temp.addFirst(arr[end]);
+            }
+            
+            if(end-start+1==windowSize)
+            {
+                max=temp.peekLast();
+                ans[start]=max;
+
+                if(arr[start]==max)
+                {
+                    temp.removeLast();
+                }
+                start++;
+            }
+            end++;
         }
         return ans;
     }

e8c2bc0326d30cccea4374451655c3dfba23d5f6
abhirup2565 <abhirup2565@gmail.com>
2025-09-03 16:00:14 +0530

    level order

---
+++ BinaryTree/LevelOrder.java
@@ -0,0 +1,110 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class LevelOrder{
+    public static void main(String[] args) {
+        Integer [] arr = {50,25,12,null,null,37,30,null,null,null,75,62,null,70,null,null,87,null,null};
+        Node root = construct(arr);
+        print(root);
+        Queue<Node> q = new ArrayDeque<>(); 
+        q.add(root);
+        while(q.size()>0)
+        {
+            int count = q.size();
+            for(int i = 0;i<count;i++)
+            {
+                Node n = q.remove();
+                System.out.print(n.data+" ");
+                if(n.left!=null)q.add(n.left);
+                if(n.right!=null)q.add(n.right);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static Node construct(Integer[] arr)
+    {
+        Node root = new Node(arr[0], null, null);
+        Pair rootPair = new Pair(root, 1);
+        Stack<Pair> st = new Stack<>();
+        st.push(rootPair);
+        int index = 0;
+        while(st.size()>0)
+        {
+            Pair top = st.peek();
+            if(top.state == 1)//left side
+            {
+                index++;
+                if(arr[index]!=null)
+                {
+                    top.node.left = new Node(arr[index], null, null);
+                    Pair leftPair = new Pair(top.node.left, 1);
+                    st.push(leftPair);
+                }
+                else
+                {
+                    top.node.left = null;
+                }
+                top.state++;
+            }
+            else if(top.state == 2)//right side
+            {
+                index++;
+                if(arr[index]!=null)
+                {
+                    top.node.right = new Node(arr[index], null, null);
+                    Pair rightPair = new Pair(top.node.right, 1);
+                    st.push(rightPair);
+                }
+                else
+                {
+                    top.node.right = null;
+                }
+                top.state++;
+            }
+            else{
+                st.pop();
+            }
+        }
+        return root;
+    }
+
+    public static void print(Node node)
+    {
+        if(node==null)
+        {
+            return;
+        }
+        String str = "";
+        str+= node.left==null?".":node.left.data;
+        str+="<-"+node.data+"->";
+        str+=node.right==null?".":node.right.data;
+        System.out.println(str);
+        print(node.left);
+        print (node.right);
+    }
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data,Node left,Node right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public static class Pair {
+        Node node;
+        int state;
+        Pair(Node node,int state)
+        {
+            this.node=node;
+            this.state=state;
+        }
+    }
+    
+}

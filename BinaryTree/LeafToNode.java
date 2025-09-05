d8c8e524601ac9a31739401964b2439278ed2957
abhirup2565 <abhirup2565@gmail.com>
2025-09-05 16:15:05 +0530

    leaf to node

---
+++ BinaryTree/LeafToNode.java
@@ -0,0 +1,106 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class LeafToNode{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer [] arr = {50,25,12,null,null,37,30,null,null,40,null,null,75,62,60,null,null,78,null,null,87,null,null};
+        Node root = construct(arr);
+        System.out.println("Enter the min");
+        int min = sc.nextInt();
+        System.out.println("Enter the max");
+        int max = sc.nextInt();
+        pathAndPrint(root,"",0,min,max);        
+    }
+
+    public static void pathAndPrint(Node node,String path,int sum,int min,int max)
+    {
+        if(node == null)
+        {
+            return;
+        }
+        if(node.left==null && node.right==null)
+        {
+            int lastSum = sum+node.data;
+             if(min<lastSum&&lastSum<max)
+            {
+                System.out.println(path+","+node.data);
+            }
+            return;
+        }
+        pathAndPrint(node.left,path+","+node.data,sum+node.data,min,max);
+        pathAndPrint(node.right,path+","+node.data,sum+node.data,min,max);
+    }
+
+    public static Node construct(Integer[] arr)
+    {
+        Node root = new Node(arr[0], null, null);
+        Pair rootPair = new Pair(root, 1);
+        Stack<Pair> st = new Stack<>();
+        st.push(rootPair);
+        int index = 0;
+        while(st.size()>0)
+        {
+            Pair top = st.peek();
+            if(top.state == 1)//left side
+            {
+                index++;
+                if(arr[index]!=null)
+                {
+                    top.node.left = new Node(arr[index], null, null);
+                    Pair leftPair = new Pair(top.node.left, 1);
+                    st.push(leftPair);
+                }
+                else
+                {
+                    top.node.left = null;
+                }
+                top.state++;
+            }
+            else if(top.state == 2)//right side
+            {
+                index++;
+                if(arr[index]!=null)
+                {
+                    top.node.right = new Node(arr[index], null, null);
+                    Pair rightPair = new Pair(top.node.right, 1);
+                    st.push(rightPair);
+                }
+                else
+                {
+                    top.node.right = null;
+                }
+                top.state++;
+            }
+            else{
+                st.pop();
+            }
+        }
+        return root;
+    }
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data,Node left,Node right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public static class Pair {
+        Node node;
+        int state;
+        Pair(Node node,int state)
+        {
+            this.node=node;
+            this.state=state;
+        }
+    }
+    
+}

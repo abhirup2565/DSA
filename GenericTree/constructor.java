45af625022c24132de92ba51c20f12010bdacd10
abhirup2565 <abhirup2565@gmail.com>
2025-09-15 18:42:40 +0530

    Generic tree

---
+++ GenericTree/constructor.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class constructor{
+    public static void main(String[] args) {
+        
+    }
+
+    public static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+}
---
+++ GenericTree/generic.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class generic {
+    public static void main(String[] args) {
+        
+    }
+
+    public static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+}

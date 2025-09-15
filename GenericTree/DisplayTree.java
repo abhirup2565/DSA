62d3c25a04600db3796ac0a32033f35382a3f2d9
abhirup2565 <abhirup2565@gmail.com>
2025-09-15 19:01:54 +0530

    generic tree display

--- GenericTree/DisplayTree.java
+++ GenericTree/DisplayTree.java
@@ -1,10 +1,25 @@
 import java.util.ArrayList;
 import java.util.Stack;
 
-public class constructor{
+public class DisplayTree{
     public static void main(String[] args) {
         int [] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
         Node root = constructor(arr);
+        display(root);
+    }
+
+    public static void display(Node node)
+    {
+        System.out.print(node.data+"->");
+        for(Node c:node.children)
+        {
+            System.out.print(c.data+", ");
+        }
+        System.out.println();
+        for(Node c:node.children)
+        {
+            display(c);
+        }
     }
 
     public static Node constructor(int[]arr)

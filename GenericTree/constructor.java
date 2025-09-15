684338c5c78885f788511b2b392620a48a734faa
abhirup2565 <abhirup2565@gmail.com>
2025-09-15 18:52:31 +0530

    constructor

--- GenericTree/constructor.java
+++ GenericTree/constructor.java
@@ -1,8 +1,38 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class constructor{
     public static void main(String[] args) {
-        
+        int [] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
+        Node root = constructor(arr);
+    }
+
+    public static Node constructor(int[]arr)
+    {
+        Node root;
+        Stack<Node> st = new Stack<>();
+        for(int i =0;i<arr.length;i++)
+        {
+            if(arr[i]==-1)
+            {
+                st.pop();
+            }
+            else
+            {
+                Node t = new Node();
+                t.data = arr[i];
+                if(st.size()>0)
+                {
+                    st.peek().children.add(t);
+                }
+                else
+                {
+                    root = t;
+                }
+                st.push(t);
+            }
+            return root;
+        }
     }
 
     public static class Node{
